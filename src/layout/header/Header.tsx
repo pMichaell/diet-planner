3eb41bc73e1ebc84b619066ee8f9d43b257f013e
pMichaell <michal.pawlowicz112@gmail.com>
2022-04-01 09:48:23 +0200

    header design started

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,7 +1,15 @@
 import classes from "./Header.module.css";
+import { ReactComponent as Checklist } from "../../assets/checklist-svgrepo-com.svg";
+import { ReactComponent as HamburgerMenu } from "../../assets/hamburgerMenu.svg";
 
 const Header = () => {
-  return <div className={classes.header}></div>;
+  return (
+    <header className={classes.header}>
+      <Checklist className={classes.checkList} />
+      <h1>Diet Planner</h1>
+      <HamburgerMenu className={classes.hamburgerMenu} />
+    </header>
+  );
 };
 
 export default Header;
