f9b0a28825a090659740b532aa252631f58ffb1b
PMichaell <michal.pawlowicz112@gmail.com>
2022-04-02 22:12:17 +0200

    children stagger in nav todo

--- src/layout/header/dropdownNav/DropdownNav.tsx
+++ src/layout/header/dropdownNav/DropdownNav.tsx
@@ -1,13 +1,31 @@
 import { motion } from "framer-motion";
 import classes from "./DropdownNav.module.css";
+import { Link } from "react-router-dom";
 
 const dropdownVariants = {
-  initial: { y: 0 },
+  initial: { height: 0, opacity: 0 },
   animate: {
-    y: "40vh",
+    height: "50vh",
+    opacity: 1,
     transition: {
       when: "beforeChildren",
+      staggerChildren: 0.5,
     },
+  },
+  exit: {
+    height: 0,
+    opacity: 0,
+  },
+};
+
+const itemVariants = {
+  initial: (i) => ({
+    opacity: 0,
+    x: i % 2 === 0 ? "100vw" : "-100vw",
+  }),
+  animate: {
+    opacity: 1,
+    x: 0,
   },
 };
 
@@ -15,10 +33,15 @@ const DropdownNav = () => {
   return (
     <motion.nav
       className={classes.nav}
-      initial={{ height: 0, opacity: 0 }}
-      animate={{ height: "50vh", opacity: 1 }}
-      exit={{ height: 0, opacity: 0 }}
-    ></motion.nav>
+      variants={dropdownVariants}
+      initial={"initial"}
+      animate={"animate"}
+      exit={"exit"}
+    >
+      <Link to={"/my-account"}>My Account</Link>
+      <Link to={"/my plans"}>My Plans</Link>
+      <Link to={"/planner"}>New Plan</Link>
+    </motion.nav>
   );
 };
 
