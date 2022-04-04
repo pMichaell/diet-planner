1a0712cbda99f1d2e83867613ad77bac0171e965
PMichaell <michal.pawlowicz112@gmail.com>
2022-04-04 09:31:33 +0200

    added icons and more animations to dropdown

--- src/layout/header/dropdownNav/DropdownNav.tsx
+++ src/layout/header/dropdownNav/DropdownNav.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "framer-motion";
 import classes from "./DropdownNav.module.css";
 import { Link, useNavigate } from "react-router-dom";
+import { Books, IconContext, ListChecks, User } from "phosphor-react";
 
 const dropdownVariants = {
   initial: { height: 0, opacity: 0.5 },
@@ -29,31 +30,66 @@ const itemVariants = {
     x: i % 2 === 0 ? "100vw" : "-100vw",
     opacity: 0,
   }),
+  whileHover: {
+    scale: 1.1,
+  },
+  whileTap: {
+    scale: 0.9,
+  },
 };
 
 const DropdownNav = () => {
   const navigate = useNavigate();
 
   return (
-    <motion.nav
-      className={classes.nav}
-      variants={dropdownVariants}
-      initial={"initial"}
-      animate={"animate"}
-      exit={"exit"}
+    <IconContext.Provider
+      value={{
+        color: "hsla(163 75% 80%)",
+        size: "2rem",
+        weight: "light",
+      }}
     >
-      <AnimatePresence>
-        <motion.div variants={itemVariants} custom={0} key={0}>
-          <Link to={"/my-account"}>My Account</Link>
-        </motion.div>
-        <motion.div variants={itemVariants} custom={1} key={1}>
-          <Link to={"/my plans"}>My Plans</Link>
-        </motion.div>
-        <motion.div variants={itemVariants} custom={2} key={2}>
-          <Link to={"/planner"}>New Plan</Link>
-        </motion.div>
-      </AnimatePresence>
-    </motion.nav>
+      <motion.nav
+        className={classes.nav}
+        variants={dropdownVariants}
+        initial={"initial"}
+        animate={"animate"}
+        exit={"exit"}
+      >
+        <AnimatePresence>
+          <motion.div
+            variants={itemVariants}
+            whileHover={"whileHover"}
+            whileTap={"whileTap"}
+            custom={0}
+            key={0}
+          >
+            <User />
+            <Link to={"/my-account"}>My Account</Link>
+          </motion.div>
+          <motion.div
+            variants={itemVariants}
+            whileHover={"whileHover"}
+            whileTap={"whileTap"}
+            custom={1}
+            key={1}
+          >
+            <Books />
+            <Link to={"/my plans"}>My Plans</Link>
+          </motion.div>
+          <motion.div
+            variants={itemVariants}
+            whileHover={"whileHover"}
+            whileTap={"whileTap"}
+            custom={2}
+            key={2}
+          >
+            <ListChecks />
+            <Link to={"/planner"}>New Plan</Link>
+          </motion.div>
+        </AnimatePresence>
+      </motion.nav>
+    </IconContext.Provider>
   );
 };
 
