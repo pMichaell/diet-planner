478ee1a1d5e7de07370ebe3a54a4da96493fd7ff
pMichaell <michal.pawlowicz112@gmail.com>
2022-04-27 10:42:38 +0200

    home page responsive design done

--- src/components/buttons/BreathingButton.tsx
+++ src/components/buttons/BreathingButton.tsx
@@ -1,21 +1,35 @@
 import { motion } from "framer-motion";
 import classes from "./BreathingButton.module.css";
 import clsx from "clsx";
+import { ReactNode } from "react";
 
 const BreathingButton = ({
   className,
   onClick,
+  children,
 }: {
   className?: string;
   onClick?: () => void;
+  children?: ReactNode;
 }) => {
   return (
     <motion.button
-      animate={{ scale: [1.0, 1.1, 1.0] }}
-      transition={{ duration: 1, repeat: Infinity, repeatType: "loop" }}
+      animate={{
+        scale: [1.0, 1.1, 1.0],
+        borderWidth: [2, 6, 2],
+      }}
+      transition={{
+        type: "tween",
+        ease: "easeInOut",
+        duration: 1,
+        repeat: Infinity,
+        repeatType: "loop",
+      }}
       className={className}
       onClick={onClick}
-    />
+    >
+      {children}
+    </motion.button>
   );
 };
 
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -18,25 +18,40 @@ const HomePage = () => {
   const [homePageActive, setHomePageActive] = useState(true);
 
   return (
-    <AnimatedPage className={"fillParent"}>
+    <AnimatedPage className={clsx("fillParent", "centerContents")}>
       <AnimatePresence>
         <motion.div className={clsx("fillParent", classes.homePage)}>
-          <motion.h2 className={clsx("uppercase", "ls1", "fontAccent")}>
-            Plan Your Meal
-          </motion.h2>
-          <motion.p className={clsx("uppercase", "fs600")}>In</motion.p>
-          <motion.h1 className={clsx("uppercase", "ls3", "fontAccent")}>
-            No Time
-          </motion.h1>
+          <motion.section className={clsx(classes.headingSection)}>
+            <motion.h2
+              className={clsx(
+                "uppercase",
+                "ls1",
+                "fontAccent",
+                "txtAlgCenter",
+                "lineHeight2",
+                "fs600"
+              )}
+            >
+              Plan Your Meal
+            </motion.h2>
+            <motion.p className={clsx("uppercase", "fs600")}>In</motion.p>
+            <motion.h1
+              className={clsx("uppercase", "ls3", "fontAccent", "fs600")}
+            >
+              No Time
+            </motion.h1>
+          </motion.section>
           <motion.p
             className={clsx("fontBody", "fw500", "fs500", "txtAlgCenter")}
           >
             {homePageText}
           </motion.p>
           <BreathingButton
-            className={clsx(classes.cta, "standardBorder")}
+            className={clsx(classes.cta, "standardBorder", "fs500")}
             onClick={() => {}}
-          />
+          >
+            Explore
+          </BreathingButton>
         </motion.div>
       </AnimatePresence>
       {!homePageActive && <PlannerDescription number={"01"} text={"LAALAL"} />}
