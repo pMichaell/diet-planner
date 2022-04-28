fa8dadf03a0e9de23fb8d43f322108c7469b2bc8
pMichaell <michal.pawlowicz112@gmail.com>
2022-04-28 12:27:26 +0200

    questionnaire page started

--- src/pages/plannerPage/questionnaire/Questionnaire.tsx
+++ src/pages/plannerPage/questionnaire/Questionnaire.tsx
@@ -2,32 +2,55 @@ import { AnimatePresence, motion } from "framer-motion";
 import classes from "./Questionnaire.module.css";
 import AnimatedPage from "../../../components/animatedPage/AnimatedPage";
 import { verticalAlignmentVariants } from "../../../framerVariants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
 const Questionnaire = () => {
   const [namingSectionVisible, setNamingSectionVisible] = useState(false);
+
+  useEffect(() => {
+    const element = document.getElementById("name");
+    const timeout = setTimeout(() => {
+      element?.focus();
+    }, 1200);
+
+    return () => window.clearTimeout(timeout);
+  });
+
   return (
     <AnimatedPage
       className={clsx(
+        "fillParent",
         "overflowHidden",
         "standardBorder",
         "backdropFilter",
+        "clrGreen",
         classes.container
       )}
     >
-      <AnimatePresence>
-        <motion.p variants={verticalAlignmentVariants} custom={3} key={2}>
-          Name your brand new plan! (You can change the name later in your
-          account page)
+      <AnimatePresence exitBeforeEnter>
+        <motion.p
+          variants={verticalAlignmentVariants}
+          custom={3}
+          key={2}
+          className={clsx("fs500", "fw500")}
+        >
+          Name your brand new plan!
         </motion.p>
         <motion.input
+          id={"name"}
           variants={verticalAlignmentVariants}
           custom={4}
           type="text"
           key={3}
+          className={clsx(classes.input, "clrGreen", "txtAlgCenter")}
         />
-        <motion.p variants={verticalAlignmentVariants} custom={1} key={0}>
+        <motion.p
+          variants={verticalAlignmentVariants}
+          custom={1}
+          key={0}
+          className={clsx(classes.questionParagraph, "fs500", "fw500")}
+        >
           How many meals per day would you like to eat?
         </motion.p>
         <motion.input
@@ -35,6 +58,7 @@ const Questionnaire = () => {
           custom={2}
           type="text"
           key={1}
+          className={clsx(classes.input, "clrGreen", "txtAlgCenter")}
         />
         {namingSectionVisible && <motion.form></motion.form>}
       </AnimatePresence>
