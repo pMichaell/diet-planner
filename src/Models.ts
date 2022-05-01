ebd20a674f2f33c182cf3243b368a72932062e25
PMichaell <michal.pawlowicz112@gmail.com>
2022-05-01 19:01:01 +0200

    added some models

--- src/Models.ts
+++ src/Models.ts
@@ -10,3 +10,27 @@ export type Weekday =
   | "FRIDAY"
   | "SATURDAY"
   | "SUNDAY";
+
+export type Meal = {
+  idMeal: number;
+  strMeal: string;
+  strMealThumb: string;
+};
+
+//measure on some index corresponds
+//to ingredient on the same index in
+//ingredients array
+export type MealDetails = {
+  meal: Meal;
+  strArea: string;
+  strCategory: string;
+  strIngredients: string[];
+  strMeasures: string[];
+  strInstructions: string;
+  strTags: string;
+  strYoutube: string;
+};
+
+export type PlanDay = {
+  [mealName: string]: Meal;
+};
